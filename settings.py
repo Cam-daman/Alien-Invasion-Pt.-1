84bad49ba574d48cc1caf84450aa26663b106172
Cam-daman <m255436@usna.edu>
2022-10-27 20:18:49 -0400

    Lesson 12 Pt 1

---
+++ settings.py
@@ -0,0 +1,8 @@
+
+class Settings():
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 500
+        self.bg_color = (230,230,230)
+
+        self.Ship_speed = 1.5
